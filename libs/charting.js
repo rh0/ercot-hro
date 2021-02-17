802856948eb452d775bb0c3e62c991aa35bd5862
rho <ryan@theoleschool.com>
2021-02-17 17:54:51 -0600

    comments

--- libs/charting.js
+++ libs/charting.js
@@ -1,21 +1,22 @@
 export default function (selector, dataSeries, key, color) {
+  // Some helpful vars.
   const width = 500,
     height = 200,
     margin = {top: 10, right: 30, bottom: 30, left: 60},
     colorMods = ['dark', '', 'light']
 
+  // Set the scales.
   let x = d3.scaleUtc()
     .domain(d3.extent(dataSeries[dataSeries.length - 1], d => d.date))
     .range([margin.left, width - margin.right])
-
   let y = d3.scaleLinear()
-    .domain([0, d3.max(dataSeries[0], d => d[key])]).nice()
+    .domain(d3.extent(dataSeries[0].concat(dataSeries[1]).concat(dataSeries[2]), d => d[key])).nice()
     .range([height - margin.bottom, margin.top])
 
+  // Define the Axes
   let xAxis = g => g
     .attr("transform", `translate(0,${height - margin.bottom})`)
     .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
-
   let yAxis = g => g
     .attr("transform", `translate(${margin.left},0)`)
     .call(d3.axisLeft(y))
@@ -26,21 +27,22 @@ export default function (selector, dataSeries, key, color) {
       .attr("font-weight", "bold")
       .text('MW'))
 
+  // Define the data line.
   let line = d3.line()
     .defined(d => !isNaN(d[key]))
     .x(d => x(d.date))
     .y(d => y(d[key]))
 
+  // Add the svg canvas.
   var svg = d3.select(selector)
     .append("svg:svg")
     .attr("viewBox", [0, 0, width, height])
-
   svg.append("g")
     .call(xAxis)
-
   svg.append("g")
     .call(yAxis)
 
+  // Draw lines for each data set.
   for(let i=0; i<dataSeries.length; i++) {
     svg.append("path")
       .datum(dataSeries[i])
